da956dc314295d2297c3b762a6abb3a45a252030
Trites <Gustaf.Svanerud@gmail.com>
2014-01-27 15:34:32 +0100

    you can now register

--- client.js
+++ client.js
@@ -5,3 +5,22 @@ function inputFocus(i){
 function inputBlur(i){
     if(i.value==""){ i.value=i.defaultValue; i.style.color="#888"; }
 }
+
+function catchError(status){
+
+	if(status.success == false){ alert(status.message); }
+}
+
+function toUserObject(email, password, firstname, familyname, gender, city, country){
+	
+	var user = new Object();
+	user.email = email;
+	user.password = password;
+	user.firstname = firstname;
+	user.familyname = familyname;
+	user.gender = gender;
+	user.city = city;
+	user.country = country;
+	
+	return user;
+}
