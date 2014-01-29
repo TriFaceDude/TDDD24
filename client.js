0adf3bf438d244feb5b3756d57012b9fc9d2b044
Trites <Gustaf.Svanerud@gmail.com>
2014-01-29 21:08:32 +0100

    added client side checks

--- client.js
+++ client.js
@@ -75,10 +75,10 @@ function toUserObject(email, password, firstname, familyname, gender, city, coun
 	return user;
 }
 
-function inputFocus(i){
-    if(i.value==i.defaultValue){ i.value=""; i.style.color="#000"; }
-}
-
 function inputBlur(i){
     if(i.value==""){ i.value=i.defaultValue; i.style.color="#888"; }
+}
+
+function inputFocus(i){
+    if(i.value==i.defaultValue){ i.value=""; i.style.color="#000"; }
 }
